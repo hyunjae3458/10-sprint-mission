6e80fa73373990b92fd65c134318ac0a8d202727
zelote123 <fredsonata@gmail.com>
2026-03-09 16:09:14 +0900

    fix: 개인채널 참가자들의 온라인상태를 userService.findUser로 동적으로 가져오도록 수정함

--- src/main/java/com/sprint/mission/discodeit/mapper/ChannelMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/ChannelMapper.java
@@ -6,6 +6,7 @@ import com.sprint.mission.discodeit.entity.BaseEntity;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import com.sprint.mission.discodeit.service.UserService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +16,14 @@ import java.util.List;
 import java.util.UUID;
 
 
-@Mapper(componentModel = "spring", uses = {UserMapper.class, ReadStatusRepository.class, MessageRepository.class})
+@Mapper(componentModel = "spring", uses = {UserService.class, ReadStatusRepository.class, MessageRepository.class})
 public abstract class ChannelMapper {
-    @Autowired
-    protected   UserMapper userMapper;
     @Autowired
     protected  ReadStatusRepository readStatusRepository;
     @Autowired
     protected  MessageRepository messageRepository;
+    @Autowired
+    private UserService userService;
 
     @Mapping(target = "lastMessageAt", expression = "java(getLatestMessageTime(channel.getId()))")
     @Mapping(target = "participants", expression = "java(getParticipants(channel.getId()))")
@@ -37,7 +38,7 @@ public abstract class ChannelMapper {
 
     protected List<UserDto> getParticipants(UUID channelId){
         return readStatusRepository.findAllByChannelId(channelId).stream()
-                .map(readStatus -> userMapper.toDto(readStatus.getUser(),true))
+                .map(readStatus -> userService.findUser(readStatus.getUser().getId()))
                 .toList();
     }
 }
