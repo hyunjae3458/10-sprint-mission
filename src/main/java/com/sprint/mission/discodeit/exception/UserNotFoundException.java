8bef18b17e55074c73b867858a121c7366d32053
zelote123 <fredsonata@gmail.com>
2026-03-26 11:43:48 +0900

    refactor: 로그에 담기위한 필드 추가

--- src/main/java/com/sprint/mission/discodeit/exception/UserNotFoundException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/UserNotFoundException.java
@@ -1,13 +1,24 @@
 package com.sprint.mission.discodeit.exception;
 
+import com.sprint.mission.discodeit.entity.User;
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class UserNotFoundException extends  RuntimeException{
+    private final String username;
+    private final UUID userId;
+
     public UserNotFoundException(String username) {
         super("User with username " + username + " not found");
+        this.username = username;
+        this.userId = null;
     }
 
     public UserNotFoundException(UUID userId) {
-        super("User with id " + userId + " not found"); // "Author with id..." 로 바꿔도 됨
+        super("User with id " + userId + " not found");
+        this.username = null;
+        this.userId = userId;
     }
 }
