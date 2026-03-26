2908d28d4721005122e05fad1d3b67ffac4a3962
zelote123 <fredsonata@gmail.com>
2026-03-26 16:38:16 +0900

    refactor: 필드 수정, 핸들러 함수 수정

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
@@ -7,11 +7,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    USER_NOT_FOUND("유저를 찾을 수 없습니다."),
-    DUPLICATE_EMAIL("중복된 이메일입니다."),
-    CHANNEL_NOT_FOUND("채널을 찾을 수 없습니다."),
-    PRIVATE_CHANNEL_UPDATE( "개인 채널은 수정할 수 없습니다."),
-    WRONG_PASSWORD( "잘못된 비밀번호입니다.");
+    USER_NOT_FOUND(404,"USER_NOT_FOUND","유저를 찾을 수 없습니다."),
+    DUPLICATE_EMAIL(409,"DUPLICATE_EMAIL","중복된 이메일입니다."),
+    CHANNEL_NOT_FOUND(404,"CHANNEL_NOT_FOUND","채널을 찾을 수 없습니다."),
+    PRIVATE_CHANNEL_UPDATE(405, "PRIVATE_CHANNEL_UPDATE", "개인 채널은 수정할 수 없습니다."),
+    WRONG_PASSWORD( 401,"WRONG_PASSWORD","잘못된 비밀번호입니다.");
 
+    private final int status;
+    private final String code;
     private final String message;
 }
--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,52 +1,32 @@
 package com.sprint.mission.discodeit.exception;
 
+import com.sprint.mission.discodeit.exception.channel.ChannelNotFoundException;
+import com.sprint.mission.discodeit.exception.channel.PrivateChannelUpdateException;
+import com.sprint.mission.discodeit.exception.user.DuplicateEmailFoundException;
+import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    @ExceptionHandler(DiscodeitException.class)
+    public ResponseEntity<ErrorResponse> handleDiscodeitException(DiscodeitException e){
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e){
-        if(e.getUserId() != null){
-            log.warn("유저 조회 실패: 조회 유저 id = {}",e.getUserId(),e);
-        } else {
-            log.warn("유저 로그인 실패: 로그인 유저 이름 = {}", e.getUsername(),e);
-        }
-        ErrorResponse response = new ErrorResponse(e.getErrorCode(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
-
-    @ExceptionHandler(WrongPasswordException.class)
-    public ResponseEntity<ErrorResponse> handleWrongPasswordException(WrongPasswordException e){
-        log.warn("잘못된 비밀번호 요청",e);
-        ErrorResponse response = new ErrorResponse(e.getErrorCode(),e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    @ExceptionHandler(ChannelNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleChannelNotFoundException(ChannelNotFoundException e){
-        log.warn("유저 조회 실패: 조회 채널 id = {}", e.getChannelId(),e);
-        ErrorResponse response = new ErrorResponse(e.getErrorCode(),e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
-
-    @ExceptionHandler(DuplicateEmailFoundException.class)
-    public ResponseEntity<ErrorResponse> handleDuplicateEmailFoundException(DuplicateEmailFoundException e){
-        log.warn("이미 사용 중인 이메일 = {}", e.getEmail(),e);
-        ErrorResponse response = new ErrorResponse(e.getErrorCode(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    @ExceptionHandler(PrivateChannelUpdateException.class)
-    public ResponseEntity<ErrorResponse> handlePrivateChannelUpdateException(PrivateChannelUpdateException e){
-        log.warn("개인 채널 수정 실패: channelId = {}", e.getChannelId(),e);
-        ErrorResponse response = new ErrorResponse(e.getErrorCode(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        log.warn("예외 발생: 메시지 = {}",e.getErrorCode().getMessage(), e);
+        ErrorResponse response = new ErrorResponse(
+                Instant.now(),
+                e.getErrorCode().getCode(),
+                e.getErrorCode().getMessage(),
+                e.getDetails(),
+                e.getClass().getSimpleName(),
+                e.getErrorCode().getStatus()
+        );
+        return ResponseEntity.status(response.getStatus()).body(response);
     }
 }
