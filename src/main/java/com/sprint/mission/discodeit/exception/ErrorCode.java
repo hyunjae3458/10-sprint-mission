d4f256fb38aeb1e4488acf2bfd77f728bbce2e4d
zelote123 <fredsonata@gmail.com>
2026-03-26 15:01:27 +0900

    feat: 에러 코드 enum타입 추가

---
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.sprint.mission.discodeit.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    USER_NOT_FOUND("유저를 찾을 수 없습니다."),
+    DUPLICATE_EMAIL("중복된 이메일입니다."),
+    CHANNEL_NOT_FOUND("채널을 찾을 수 없습니다."),
+    PRIVATE_CHANNEL_UPDATE( "개인 채널은 수정할 수 없습니다."),
+    WRONG_PASSWORD( "잘못된 비밀번호입니다.");
+
+    private final String message;
+}
