f5a3a2b746c5fbf3ac150a070c28aa09f9d999c7
zelote123 <fredsonata@gmail.com>
2026-03-26 13:20:09 +0900

    refactor: 유저 컨트롤러 로그 추가

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,7 @@ import java.util.UUID;
 @RequestMapping("/api/users")
 @Tag(name = "User", description = "User API")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
     private final UserStatusService userStatusService;
@@ -35,6 +37,8 @@ public class UserController {
     public ResponseEntity<UserDto> postUser(@Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
                                             @RequestPart("userCreateRequest") UserCreateRequest request,
                                             @RequestPart(value = "profile", required = false)MultipartFile profile){
+        log.debug("유저 회원가입 요청: 유저 이름 = {}, 유저 이메일 = {}, 프로필 여부 = {}",
+                request.getUsername(), request.getEmail(), profile != null);
         UserDto response = userService.create(request , profile);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
@@ -59,6 +63,7 @@ public class UserController {
                                               @PathVariable("userId") UUID id,
                                               @RequestPart("userUpdateRequest") UserUpdateRequest request,
                                               @RequestPart(value="profile", required = false) MultipartFile profile){
+        log.debug("유저 정보 수정 요청: 수정할 유저 id = {}, 프로필 수정 여부 = {}",id, profile != null);
         UserDto response = userService.update(id,request, profile);
         return ResponseEntity.ok(response);
     }
@@ -75,6 +80,7 @@ public class UserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteUser(@PathVariable("userId") UUID id){
+        log.debug("유저 삭제 요청: 삭제할 유저 id = {}",id);
         userService.delete(id);
         return ResponseEntity.noContent().build(); // noContent는 객체를 만든다는 뜻, build를 붙혀서 객체 만듬
 
