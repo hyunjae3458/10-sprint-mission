6ed0d6c3ac1b96937f23127736c99bc836d105ef
zelote123 <fredsonata@gmail.com>
2026-03-26 17:48:32 +0900

    refactor: 부모 객체 변경

--- src/main/java/com/sprint/mission/discodeit/exception/readStatus/ReadStatusNotFoundException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/readStatus/ReadStatusNotFoundException.java
@@ -5,7 +5,7 @@ import com.sprint.mission.discodeit.exception.ErrorCode;
 import java.util.Map;
 import java.util.UUID;
 
-public class ReadStatusNotFoundException extends RuntimeException {
+public class ReadStatusNotFoundException extends ReadStatusException {
     public ReadStatusNotFoundException(UUID readStatusId) {
         super(ErrorCode.READSTATUS_NOT_FOUND, Map.of("readStatusId", readStatusId));
     }
