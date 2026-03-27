f6e21fcc229177b4f9224ca188e3807add8f6ec5
zelote123 <fredsonata@gmail.com>
2026-03-27 11:05:56 +0900

    fix: 채널 테이블의 타입 컬럼에 Not Null수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE channels
     id           UUID PRIMARY KEY,
     channel_name VARCHAR(100),
     description  VARCHAR(500),
-    channel_type VARCHAR(20) CHECK (channel_type IN ('PRIVATE', 'PUBLIC')) NULL,
+    channel_type VARCHAR(20) CHECK (channel_type IN ('PRIVATE', 'PUBLIC')) NOT NULL,
     created_at   TIMESTAMPTZ   NOT NULL,
     updated_at   TIMESTAMPTZ
 );
