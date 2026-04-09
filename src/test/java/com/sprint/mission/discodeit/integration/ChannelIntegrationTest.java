df54b7f789ca985c7d4e6f597d97934789af6bb9
zelote123 <fredsonata@gmail.com>
2026-04-09 15:30:18 +0900

    fix: channel 통합 테스트 transactional 추가

--- src/test/java/com/sprint/mission/discodeit/integration/ChannelIntegrationTest.java
+++ src/test/java/com/sprint/mission/discodeit/integration/ChannelIntegrationTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class ChannelIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
